2e538359c57d666f41ec9eb002225d8bfee1baf0
Alireza Nikandish <rralireza254@yahoo.com>
2025-07-25 20:25:29 +0330

    Add Command and CommandHandler interfaces

---
+++ RetroGameLog.Application/Abstractions/Messaging/ICommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using RetroGameLog.Domain.Abstractions;
+
+namespace RetroGameLog.Application.Abstractions.Messaging;
+
+public interface ICommand : IRequest<Result>, IBaseCommand
+{
+}
+
+
+public interface ICommand<TResponse> : IRequest<Result<TResponse>>, IBaseCommand
+{
+}
+
+public interface IBaseCommand
+{
+}
