04fe9e4db1ba96e3f32220a30325f6f8d14c253e
Alireza Nikandish <rralireza254@yahoo.com>
2025-07-25 10:10:20 +0330

    Add application layer to project and IServiceCollection method for inject services!

---
+++ RetroGameLog.Application/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace RetroGameLog.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection StartUpConfiguration(this IServiceCollection services)
+    {
+        //Inject MediatR
+        services.AddMediatR(configuration =>
+        {
+            configuration.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+        });
+
+        return services;
+    }
+}
