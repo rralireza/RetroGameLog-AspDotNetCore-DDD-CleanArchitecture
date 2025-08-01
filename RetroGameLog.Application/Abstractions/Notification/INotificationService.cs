8e27a8586eacc0df45b939ebb5942e220678b5d6
Alireza Nikandish <rralireza254@yahoo.com>
2025-08-01 10:01:30 +0330

    Add game create domain event handler to application layer

---
+++ RetroGameLog.Application/Abstractions/Notification/INotificationService.cs
@@ -0,0 +1,6 @@
+﻿namespace RetroGameLog.Application.Abstractions.Notification;
+
+public interface INotificationService
+{
+    Task SendAsync(string status, string message);
+}
---
+++ RetroGameLog.Application/Games/CreateGame/GameCreatedDomainEventHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using RetroGameLog.Application.Abstractions.Notification;
+using RetroGameLog.Domain.Games;
+using RetroGameLog.Domain.Games.Events;
+
+namespace RetroGameLog.Application.Games.CreateGame;
+
+internal sealed class GameCreatedDomainEventHandler : INotificationHandler<GameCreatedDomainEvent>
+{
+    private readonly IGameRepository _gameRepository;
+
+    private readonly INotificationService _notification;
+
+    public GameCreatedDomainEventHandler(IGameRepository gameRepository, INotificationService notification)
+    {
+        _gameRepository = gameRepository;
+        _notification = notification;
+    }
+
+    public async Task Handle(GameCreatedDomainEvent notification, CancellationToken cancellationToken)
+    {
+        var game = await _gameRepository.GetByIdAsync(notification.GameId, cancellationToken);
+
+        if (game == null)
+            return;
+
+        var message = $"{game.Title} has been created successfully.";
+
+        await _notification.SendAsync("Success", message);
+    }
+}
