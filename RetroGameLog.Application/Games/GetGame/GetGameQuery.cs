6bbe7d839abed2541cd9273c26d03ad185102541
Alireza Nikandish <rralireza254@yahoo.com>
2025-08-05 19:28:28 +0330

    Add GetGameQuery to project

---
+++ RetroGameLog.Application/Games/GetGame/GetGameQuery.cs
@@ -0,0 +1,5 @@
+﻿using RetroGameLog.Application.Abstractions.Messaging;
+
+namespace RetroGameLog.Application.Games.GetGame;
+
+public sealed record GetGameQuery(Guid GameId) : IQuery<GameResponseDto>;
---
+++ RetroGameLog.Application/Games/GetGame/GetGameQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using Dapper;
+using RetroGameLog.Application.Abstractions.Data;
+using RetroGameLog.Application.Abstractions.Messaging;
+using RetroGameLog.Domain.Abstractions;
+
+namespace RetroGameLog.Application.Games.GetGame;
+
+internal sealed class GetGameQueryHandler : IQueryHandler<GetGameQuery, GameResponseDto>
+{
+    private readonly ISqlConnectionFactory _sqlConnectionFactory;
+
+    public GetGameQueryHandler(ISqlConnectionFactory sqlConnectionFactory)
+    {
+        _sqlConnectionFactory = sqlConnectionFactory;
+    }
+
+    public async Task<Result<GameResponseDto>> Handle(GetGameQuery request, CancellationToken cancellationToken)
+    {
+        using var connection = _sqlConnectionFactory.CreateConnection();
+
+        const string query = $"""
+                              SELECT
+                                id AS Id,
+                                title AS Title,
+                                platform AS Platform,
+                                releaseYear AS ReleaseYear,
+                                genre AS Genre,
+                                developer AS Developer,
+                              FROM games
+                              WHERE id = @gameId
+                              """;
+
+        GameResponseDto? game = await connection.QueryFirstOrDefaultAsync(query, new { request.GameId });
+
+        return game;
+    }
+}
