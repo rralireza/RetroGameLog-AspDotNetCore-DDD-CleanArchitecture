b11d6cbfb22f048e7d10bdcd36e56434e8485c79
Alireza Nikandish <rralireza254@yahoo.com>
2025-08-05 20:20:16 +0330

    Add user create domain event handler

---
+++ RetroGameLog.Application/Users/CreateUser/UserCreatedDomainEventHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using RetroGameLog.Application.Abstractions.Notification;
+using RetroGameLog.Domain.Users;
+using RetroGameLog.Domain.Users.Events;
+
+namespace RetroGameLog.Application.Users.CreateUser;
+
+internal sealed class UserCreatedDomainEventHandler : INotificationHandler<UserCreatedDomainEvent>
+{
+    private readonly IUserRepository _userRepository;
+
+    private readonly INotificationService _notificationService;
+
+    public UserCreatedDomainEventHandler(IUserRepository userRepository, INotificationService notificationService)
+    {
+        _userRepository = userRepository;
+        _notificationService = notificationService;
+    }
+
+    public async Task Handle(UserCreatedDomainEvent notification, CancellationToken cancellationToken)
+    {
+        var user = await _userRepository.GetByIdAsync(notification.UserId, cancellationToken);
+
+        if (user == null)
+            return;
+
+        string message = "User has been created successfully.";
+
+        await _notificationService.SendAsync("Success", message);
+    }
+}
