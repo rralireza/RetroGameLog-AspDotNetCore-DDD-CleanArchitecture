fc352f5e8fd18376caa2d63bdedcf5ffc64e8207
Alireza Nikandish <rralireza254@yahoo.com>
2025-08-14 20:28:14 +0330

    Add Infrastructure layer to solution

---
+++ RetroGameLog.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RetroGameLog.Application.Abstractions.Notification;
+using RetroGameLog.Infrastructure.Notification;
+
+namespace RetroGameLog.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection RegisterToIoc(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddTransient<INotificationService, NotificationService>();
+
+        return services;
+    }
+}
---
+++ RetroGameLog.Infrastructure/Notification/NotificationService.cs
@@ -0,0 +1,11 @@
+﻿using RetroGameLog.Application.Abstractions.Notification;
+
+namespace RetroGameLog.Infrastructure.Notification;
+
+internal sealed class NotificationService : INotificationService
+{
+    public Task SendAsync(string status, string message)
+    {
+        return Task.CompletedTask; // No-op implementation
+    }
+}
