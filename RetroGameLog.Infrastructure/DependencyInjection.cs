64ad27da2a7cc165be97467b93bb2ffb18b2163f
Alireza Nikandish <rralireza254@yahoo.com>
2025-08-15 15:25:29 +0330

    Register DbContext to IOC container

--- RetroGameLog.Infrastructure/DependencyInjection.cs
+++ RetroGameLog.Infrastructure/DependencyInjection.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using RetroGameLog.Application.Abstractions.Notification;
+using RetroGameLog.Infrastructure.DatabaseContext;
 using RetroGameLog.Infrastructure.Notification;
 
 namespace RetroGameLog.Infrastructure;
@@ -10,6 +12,13 @@ public static class DependencyInjection
     public static IServiceCollection RegisterToIoc(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddTransient<INotificationService, NotificationService>();
+
+        var connectionString = configuration.GetConnectionString("DatabaseConnection") ?? throw new ArgumentNullException(nameof(configuration));
+
+        services.AddDbContext<RetroGameLogDbContext>(options =>
+        {
+            options.UseSqlServer(connectionString);
+        });
 
         return services;
     }
