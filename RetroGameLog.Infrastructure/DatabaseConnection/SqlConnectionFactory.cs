a2a0b88e27dbcbcb34c711ca3dbee95ff9a48b98
Alireza Nikandish <rralireza254@yahoo.com>
2025-08-23 08:07:27 +0330

    Implement sqlConnectionFactory class

---
+++ RetroGameLog.Infrastructure/DatabaseConnection/SqlConnectionFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Data.SqlClient;
+using RetroGameLog.Application.Abstractions.Data;
+using System.Data;
+
+namespace RetroGameLog.Infrastructure.DatabaseConnection;
+
+internal sealed class SqlConnectionFactory : ISqlConnectionFactory
+{
+    private readonly string _connectionString;
+
+    public SqlConnectionFactory(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+    public IDbConnection CreateConnection()
+    {
+        var connection = new SqlConnection(_connectionString);
+
+        connection.Open();
+
+        return connection;
+    }
+}
