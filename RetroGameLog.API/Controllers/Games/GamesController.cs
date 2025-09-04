6e3d3c0e3299636700aec0d91aefb6a1e3e839b6
Alireza Nikandish <rralireza254@yahoo.com>
2025-09-04 21:32:30 +0330

    Add GameController to presentation layer

---
+++ RetroGameLog.API/Controllers/Games/GamesController.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using RetroGameLog.Application.Games.GetAllGames;
+
+namespace RetroGameLog.API.Controllers.Games;
+
+[Route("api/[controller]")]
+[ApiController]
+public class GamesController : ControllerBase
+{
+    private readonly ISender _sender;
+
+    public GamesController(ISender sender)
+    {
+        _sender = sender;
+    }
+
+    [HttpGet("GetAllGames")]
+    public async Task<IActionResult> GetAllGames(CancellationToken cancellationToken)
+    {
+        var query = new GetAllGamesQuery();
+
+        var result = await _sender.Send(query, cancellationToken);
+
+        return Ok(result.Value);
+    }
+}
