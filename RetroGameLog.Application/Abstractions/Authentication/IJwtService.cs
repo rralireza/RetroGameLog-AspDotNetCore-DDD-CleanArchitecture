6c86f87c3451d4998b3a2af09b89d9a1556dd0df
Alireza Nikandish <rralireza254@yahoo.com>
2025-12-25 10:33:42 +0330

    add JwtService contracts and LoginUserCommandHandler

---
+++ RetroGameLog.Application/Abstractions/Authentication/IJwtService.cs
@@ -0,0 +1,8 @@
+﻿using RetroGameLog.Domain.Abstractions;
+
+namespace RetroGameLog.Application.Abstractions.Authentication;
+
+public interface IJwtService
+{
+    Task<Result<string>> GenerateTokenAsync(string email, string password, CancellationToken cancellationToken = default);
+}
---
+++ RetroGameLog.Application/Users/LoginUser/LoginUserCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using RetroGameLog.Application.Abstractions.Authentication;
+using RetroGameLog.Application.Abstractions.Messaging;
+using RetroGameLog.Domain.Abstractions;
+
+namespace RetroGameLog.Application.Users.LoginUser;
+
+internal sealed class LoginUserCommandHandler : ICommandHandler<LoginUserCommand, AccessTokenResponse>
+{
+    private readonly IJwtService _jwtService;
+
+    public LoginUserCommandHandler(IJwtService jwtService)
+    {
+        _jwtService = jwtService;
+    }
+
+    public async Task<Result<AccessTokenResponse>> Handle(LoginUserCommand request, CancellationToken cancellationToken)
+    {
+        var generateToken = await _jwtService.GenerateTokenAsync(request.Email, request.Password, cancellationToken);
+
+        if (generateToken.IsFailure)
+            return Result.Failure<AccessTokenResponse>(Error.BadRequest);
+
+        var result = new AccessTokenResponse(generateToken.Value);
+
+        return result;
+    }
+}
