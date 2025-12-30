72e86ba7b3b5c012d94af436234efac925bd74cf
Alireza Nikandish <rralireza254@yahoo.com>
2025-12-30 19:34:36 +0330

    implement PermissionAuthorizationPolicyProvider

---
+++ RetroGameLog.Infrastructure/Authorization/PermissionAuthorizationPolicyProvider.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Options;
+
+namespace RetroGameLog.Infrastructure.Authorization;
+
+internal sealed class PermissionAuthorizationPolicyProvider : DefaultAuthorizationPolicyProvider
+{
+    private readonly AuthorizationOptions _options;
+    public PermissionAuthorizationPolicyProvider(IOptions<AuthorizationOptions> options) : base(options)
+    {
+        _options = options.Value;
+    }
+
+    public override async Task<AuthorizationPolicy?> GetPolicyAsync(string policyName)
+    {
+        var policy = await base.GetPolicyAsync(policyName);
+
+        if (policy is not null)
+            return policy;
+
+        var permissionPolicy = new AuthorizationPolicyBuilder()
+            .AddRequirements(new PermissionRequirements(policyName))
+            .Build();
+
+        _options.AddPolicy(policyName, permissionPolicy);
+
+        return permissionPolicy;
+    }
+}
