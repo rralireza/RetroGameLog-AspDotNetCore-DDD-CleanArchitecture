fa2effb1304c8fca3c0de340068ba949fc3e1f5a
Alireza Nikandish <rralireza254@yahoo.com>
2025-12-30 18:08:09 +0330

    add Permission entity

---
+++ RetroGameLog.Domain/Users/Permission.cs
@@ -0,0 +1,18 @@
+﻿namespace RetroGameLog.Domain.Users;
+
+public sealed class Permission
+{
+
+    public static readonly Permission UserCanRead = new(1, "User.Read");
+    public static readonly Permission UserCanReadAndWrite = new(2, "User.ReadAndWrite");
+
+    public Permission(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+
+    public int Id { get; init; }
+
+    public string Name { get; init; } = string.Empty;
+}
