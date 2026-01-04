c64432fb5b66dc99f9010fb2de212f83b77f3ff1
Alireza Nikandish <rralireza254@yahoo.com>
2026-01-04 21:32:09 +0330

    add Caching abstraction

---
+++ RetroGameLog.Application/Abstractions/Caching/ICacheService.cs
@@ -0,0 +1,10 @@
+﻿namespace RetroGameLog.Application.Abstractions.Caching;
+
+public interface ICacheService
+{
+    Task<T?> GetAsync<T>(string key, CancellationToken cancellationToken = default);
+
+    Task<T> SetAsync<T>(string key, T value, TimeSpan? absoluteExpirationRelativeToNow = null, CancellationToken cancellationToken = default);
+
+    Task RemoveAsync(string key, CancellationToken cancellationToken = default);
+}
