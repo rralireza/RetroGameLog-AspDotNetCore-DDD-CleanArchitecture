03f12929e7e5106e7d91bb566d66d37c1d59e309
Alireza Nikandish <rralireza254@yahoo.com>
2026-05-27 21:10:17 +0330

    add outbox options for configuration processing background jobs

---
+++ RetroGameLog.Infrastructure/Outbox/OutboxOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace RetroGameLog.Infrastructure.Outbox;
+
+internal sealed class OutboxOptions
+{
+    public int IntervalInSeconds { get; set; } //How long does it take to background job process?
+
+    public int BatchSize { get; set; } //How many messages should be processed in one batch?
+}
