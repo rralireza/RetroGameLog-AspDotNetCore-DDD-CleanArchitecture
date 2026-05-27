bfaf35d5a9913f91f321e7ae97243459d31f5e79
Alireza Nikandish <rralireza254@yahoo.com>
2026-05-27 21:52:00 +0330

    implementing outbox messages processing setup class

---
+++ RetroGameLog.Infrastructure/Outbox/ProcessOutboxMessagesJobSetup.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Options;
+using Quartz;
+
+namespace RetroGameLog.Infrastructure.Outbox;
+
+internal sealed class ProcessOutboxMessagesJobSetup : IConfigureOptions<QuartzOptions>
+{
+    private readonly OutboxOptions _outboxOptions;
+
+    public ProcessOutboxMessagesJobSetup(IOptions<OutboxOptions> outboxOptions)
+    {
+        _outboxOptions = outboxOptions.Value;
+    }
+
+    public void Configure(QuartzOptions options)
+    {
+        const string jobName = nameof(ProcessOutboxMessagesJobSetup);
+
+        options
+            .AddJob<ProcessOutboxMessagesJob>(configure => configure.WithIdentity(jobName))
+            .AddTrigger(configure => 
+                configure
+                .ForJob(jobName)
+                .WithSimpleSchedule(schedule => schedule.WithIntervalInSeconds(_outboxOptions.IntervalInSeconds).RepeatForever()));
+    }
+}
